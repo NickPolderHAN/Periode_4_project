2f5b156cdad4664d3a49d5debe585ed0cdcccc62
NickPolderHAN <n.vandepolder@student.han.nl>
2022-05-10 11:18:31 +0200

    v1.6 - fixed the Blast script.

--- Python/Blast_script.py
+++ Python/Blast_script.py
@@ -3,15 +3,27 @@ import time
 
 
 def blast_dictionary(key, sequence):
-    time.sleep(10.0)
     print("Start BLAST on: " + key)
     result_handle = NCBIWWW.qblast("blastx", "nr", sequence,
                                    hitlist_size=10)
 
-    return result_handle
+    with open("my_blastx.xml", "w") as out_handle:
+        out_handle.write(result_handle.read())
+
+    with open("my_blastx.xml", "r") as out_handle:
+        blast_record = NCBIXML.parse(out_handle)
+        blast_record = next(blast_record)
+        print(blast_record)
+        eval_thresh = 0.04
+        for alignment in blast_record.alignments:
+            for hsp in alignment.hsps:
+                print("***Alignment***")
+                print("Sequence:", alignment.title)
+                print("Length:", alignment.length)
+                print("E value", hsp.expect)
 
 def main(sequence_dictionary):
     for key in sequence_dictionary:
         sequence = sequence_dictionary[key][0]
-        results = blast_dictionary(key, sequence)
-        print(results)
+        blast_dictionary(key, sequence)
+        time.sleep(20.0)
