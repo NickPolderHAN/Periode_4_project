decc13f79969d3ef8157ba291f9de81089dfe95d
NickPolderHAN <n.vandepolder@student.han.nl>
2022-05-18 10:10:29 +0200

    v2.1 - Started working on the process xml file script. - Started working on the insert to database script.

---
+++ Python/SQL_connector_script.py
@@ -0,0 +1,10 @@
+import mysql.connector
+import process_blast_results
+
+
+def insert_to_database():
+    pass
+
+
+def main():
+    process_blast_results.BlastParser()
--- Python/process_blast_results.py
+++ Python/process_blast_results.py
@@ -1,18 +1,34 @@
 from Bio.Blast import NCBIXML
+import time
 
 
-with open("my_blastx.xml", "r") as out_handle:
-    blast_record = NCBIXML.parse(out_handle)
-    blast_record = next(blast_record)
-    print(blast_record)
-    eval_thresh = 0.04
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            print("***Alignment***")
-            print("Sequence:", alignment.title)
-            print("Length:", alignment.length)
-            print("E value", hsp.expect)
+# used to retrieve the Blast results from a xml file.
+class BlastParser:
+    """
+    BlastParser takes an amount of files and uses this amount in a for
+    loop to concatenate the file_number with "my_blastx" and ".xml" to get
+    the file name. After processing the files it returns them one by one.
 
+    required parameters:
+    files_amount: The amount of files named my_blast[num].xml.
+    """
+    def __int__(self, files_amount):
+        self.__counter = 1
+        self.__file_count = files_amount
 
-def main():
-    pass
+    def read_xml_file(self):
+        while self.__counter <= self.__file_count:
+            file_name = "my_blastx" + str(self.__counter) + ".xml"
+            with open(file_name, "r") as out_handle:
+                time.sleep(10)
+                blast_record = NCBIXML.parse(out_handle)
+                blast_record = next(blast_record)
+                print(blast_record)
+                for alignment in blast_record.alignments:
+                    for hsp in alignment.hsps:
+                        print("\n")
+                        print("***Alignment***")
+                        print("Sequence:", alignment.title)
+                        print("Length:", alignment.length)
+                        print("E value", hsp.expect)
+                self.__counter += 1
