96e99f0c78c4b0b80b90f9094bb494cce11322a0
AmbraHovenier <a.hovenier@student.han.nl>
2022-05-23 10:18:25 +0200

    Update Opzet_html.py
    De table.html en sorteerknoppen.py gekoppeld aan opzet_html.py

--- Python/Opzet_html.py
+++ Python/Opzet_html.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from Python.sorteerknoppen import sorteerknoppen
 
 app = Flask(__name__)
 
@@ -21,6 +22,20 @@ def blast_pagina():
 @app.route("/database_inladen", methods=["POST", "GET"])
 def database_inladen():
     return
+
+@app.route("/table", methods=["POST", "GET"])
+def table():
+    sk = sorteerknoppen()
+    if request.method == "POST":
+        e_value_min = str(request.form.get("e_value_min").strip().replace('\n', ''))
+        e_value_max = str(request.form.get("e_value_max").strip().replace('\n', ''))
+        percent_identity_min = str(request.form.get("percent_identity_min").strip().replace('\n', ''))
+        percent_identity_max = str(request.form.get("percent_identity_max").strip().replace('\n', ''))
+        organism = str(request.form.get("organism").strip().replace('\n', ''))
+        protein = str(request.form.get("protein").strip().replace('\n', ''))
+        result = sk.get_data(e_value_min, e_value_max, percent_identity_min,
+                             percent_identity_max, organism, protein)
+    return render_template("table.html", data=result)
 
 
 if __name__ == '__main__':
