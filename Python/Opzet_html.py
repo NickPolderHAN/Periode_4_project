c9636fae26a0fa0ba793c5a79eeb3b5d855256d5
AmbraHovenier <a.hovenier@student.han.nl>
2022-05-24 10:46:47 +0200

    Update Opzet_html.py
    Alle verwijzingen naar de betreffende pagina's werkend gemaakt.

--- Python/Opzet_html.py
+++ Python/Opzet_html.py
@@ -1,42 +1,43 @@
 from flask import Flask, render_template, request
 from Python.sorteerknoppen import sorteerknoppen
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 
-# @app.route("/", methods=["POST", "GET"])
-# def hoofdpagina():
-#     return render_template('template/cover/index.html')
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 
-# @app.route("/resultaten", methods=["POST", "GET"])
-# def resultaten():
-#     return render_template('graphs.html')
-#
-#
-# @app.route("/infopagina", methods=["POST", "GET"])
-# def blast_pagina():
-#     return render_template('learnmore.html')
-#
+@app.route("/graphs", methods=["POST", "GET"])
+def graphs():
+    return render_template('graphs.html')
+
+
+@app.route("/infopagina", methods=["POST", "GET"])
+def learn_more():
+    return render_template('learnmore.html')
+
 
 @app.route("/database_inladen", methods=["POST", "GET"])
-def database_inladen():
+def table():
     return render_template('table.html')
-#
-# @app.route("/table", methods=["POST", "GET"])
-# def table():
-#     sk = sorteerknoppen()
-#     if request.method == "POST":
-#         e_value_min = str(request.form.get("e_value_min").strip().replace('\n', ''))
-#         e_value_max = str(request.form.get("e_value_max").strip().replace('\n', ''))
-#         percent_identity_min = str(request.form.get("percent_identity_min").strip().replace('\n', ''))
-#         percent_identity_max = str(request.form.get("percent_identity_max").strip().replace('\n', ''))
-#         organism = str(request.form.get("organism").strip().replace('\n', ''))
-#         protein = str(request.form.get("protein").strip().replace('\n', ''))
-#         result = sk.get_data(e_value_min, e_value_max, percent_identity_min,
-#                              percent_identity_max, organism, protein)
-#     return render_template("table.html", data=result)
-#
+
+
+@app.route("/table", methods=["POST", "GET"])
+def filter_resultaten():
+    sk = sorteerknoppen()
+    if request.method == "POST":
+        e_value_min = str(request.form.get("e_value_min").strip().replace('\n', ''))
+        e_value_max = str(request.form.get("e_value_max").strip().replace('\n', ''))
+        percent_identity_min = str(request.form.get("percent_identity_min").strip().replace('\n', ''))
+        percent_identity_max = str(request.form.get("percent_identity_max").strip().replace('\n', ''))
+        organism = str(request.form.get("organism").strip().replace('\n', ''))
+        protein = str(request.form.get("protein").strip().replace('\n', ''))
+        result = sk.get_data(e_value_min, e_value_max, percent_identity_min,
+                             percent_identity_max, organism, protein)
+    return render_template("table.html", data=result)
+
 
 if __name__ == '__main__':
     app.run()
