fcfd37aae4005699e1148f31a91cac5adb454465
EsmayW <90254276+EsmayW@users.noreply.github.com>
2022-05-28 14:25:39 +0200

    De grafiek_top_10.py in een class gezet en commentaar toe gevoegd. De Opzet_html.py bijgewerkt dat de data in verschillende strings staan zodat het in de html makkelijker in te vullen is.

--- Python/Opzet_html.py
+++ Python/Opzet_html.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from Python.sorteerknoppen import SorteerKnoppen
+from Python.grafiek_top_10 import top10
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__, template_folder='../templates',
+            static_folder='../static')
 
 
 @app.route("/")
@@ -11,6 +13,14 @@ def index():
 
 @app.route("/graphs", methods=["POST", "GET"])
 def graphs():
+    grafiek = top10()
+    eiwitten, aantal_eiwit = grafiek.data_top10_eiwitten()
+    organismen, aantal_organisme = grafiek.data_top10_organismen()
+    ei1, ei2, ei3, ei4, ei5, ei6, ei7, ei8, ei9, ei10, = eiwitten
+    cei1, cei2, cei3, cei4, cei5, cei6, cei7, cei8, cei9, cei10 = aantal_eiwit
+    org1, org2, org3, org4, org5, org6, org7, org8, org9, org10 = organismen
+    corg1, corg2, corg3, corg4, corg5, corg6, corg7, corg8, corg9, \
+    corg10 = aantal_organisme
     return render_template('graphs.html')
 
 
@@ -29,10 +39,14 @@ def filter_resultaten():
     sk = SorteerKnoppen()
 
     if request.method == "POST":
-        e_value_min = str(request.form.get("e_value_min").strip().replace('\n', ''))
-        e_value_max = str(request.form.get("e_value_max").strip().replace('\n', ''))
-        percent_identity_min = str(request.form.get("percent_identity_min").strip().replace('\n', ''))
-        percent_identity_max = str(request.form.get("percent_identity_max").strip().replace('\n', ''))
+        e_value_min = str(
+            request.form.get("e_value_min").strip().replace('\n', ''))
+        e_value_max = str(
+            request.form.get("e_value_max").strip().replace('\n', ''))
+        percent_identity_min = str(
+            request.form.get("percent_identity_min").strip().replace('\n', ''))
+        percent_identity_max = str(
+            request.form.get("percent_identity_max").strip().replace('\n', ''))
         organism = str(request.form.get("organism").strip().replace('\n', ''))
         protein = str(request.form.get("protein").strip().replace('\n', ''))
         result = sk.get_data(e_value_min, e_value_max, percent_identity_min,
--- Python/grafiek_top_10.py
+++ Python/grafiek_top_10.py
@@ -1,76 +1,75 @@
 import mysql.connector as mq
 
 
-def data_top10_eiwitten():
-    """Maakt connectie met de database en haalt de top 10 eiwitten uit de
-      database. Deze resultaten voegt die toe aan een lijst.
+class top10():
+    def __init__(self):
+        self.__eiwit = []
+        self.__aantal_eiwit = []
+        self.__organisme = []
+        self.__aantal_organsime = []
 
-    :return:
-    eiwit -> lijst -> Staan alle eiwitten in van de sql statement
-    aantal_eiwit -> lijst -> Staan alle aantallen in van de sql statement
-    """
-    db = mq.connect(host="145.74.104.145",
-                    user="pwtit",
-                    password="pwd123",
-                    database="pwtit",
-                    auth_plugin='mysql_native_password'
-                    )
-    dbcursor = db.cursor()
-    #Haalt de top 10 eiwitten uit de database
-    eiwitten = "Select eiwit, count(*) From hits Group by eiwit " \
-               "Order by count(* ) desc limit 10"
-    dbcursor.execute(eiwitten)
-    eiwit_counter = dbcursor.fetchall()
-    eiwit = []
-    aantal_eiwit = []
-    #Leest de resultaten door en voegt alles to aan de juiste lijsten
-    for results in eiwit_counter:
-        counter = 0
-        for result in results:
-            if counter == 0:
-                counter += 1
-                eiwit.append(result)
-            elif counter == 1:
-                aantal_eiwit.append(result)
-    return eiwit, aantal_eiwit
+    def data_top10_eiwitten(self):
+        """
+        Haalt de top 10 eiwitten met het aantal daarvan uit de database
+        en returnd twee lijsten met de data van de database.
+        :return:
+        eiwit -> list -> De top 10 eiwitten uit de database.
+        aantal_eiwit -> list -> Het aantal van de top 10 eiwitten
+                                uit de database.
+        """
+        # Maakt een connectie met de database.
+        db = mq.connect(host="145.74.104.145",
+                        user="pwtit",
+                        password="pwd123",
+                        database="pwtit",
+                        auth_plugin='mysql_native_password'
+                        )
+        dbcursor = db.cursor()
+        # Haalt de top 10 eiwitten uit de database
+        eiwitten = "Select eiwit, count(*) From hits Group by eiwit " \
+                   "Order by count(* ) desc limit 10"
+        dbcursor.execute(eiwitten)
+        eiwit_counter = dbcursor.fetchall()
+        # Zet de resultaten in de goede lijsten
+        for results in eiwit_counter:
+            counter = 0
+            for result in results:
+                if counter == 0:
+                    counter += 1
+                    self.__eiwit.append(result)
+                elif counter == 1:
+                    self.__aantal_eiwit.append(result)
+        return self.__eiwit, self.__aantal_eiwit
 
-
-def data_top10_organismen():
-    """Maakt connectie met de database en haalt de top 10 organismen uit de
-      database. Deze resultaten voegt die toe aan een lijst.
-
-    :return:
-    organisme -> lijst -> Staan alle organismen in van de sql statement
-    aantal_organisme -> lijst -> Staan alle aantallen in van de sql statement
-    """
-    #Maakt connectie met de database
-    db = mq.connect(host="145.74.104.145",
-                    user="pwtit",
-                    password="pwd123",
-                    database="pwtit",
-                    auth_plugin='mysql_native_password'
-                    )
-    dbcursor = db.cursor()
-    #Haalt de top 10 organismen uit de database
-    organismen = "Select organisme, count(*) From hits Group by organisme " \
-                 "Order by count(*) desc limit 10"
-    dbcursor.execute(organismen)
-    organisme_counter = dbcursor.fetchall()
-    organisme = []
-    aantal_organsime = []
-    #Leest de resultaten door en voegt alles to aan de juiste lijsten
-    for results in organisme_counter:
-        counter = 0
-        for result in results:
-            if counter == 0:
-                counter += 1
-                organisme.append(result)
-            elif counter == 1:
-                aantal_organsime.append(result)
-    return organisme, aantal_organsime
-
-
-def main():
-    eiwit, aantal_eiwit = data_top10_eiwitten()
-    organisme, aantal_organsime = data_top10_organismen()
-    return eiwit, aantal_eiwit, organisme, aantal_organsime
+    def data_top10_organismen(self):
+        """
+        Haalt de top 10 organismen met het aantal daarvan uit de database
+        en returnd twee lijsten met de data van de database.
+        :return:
+        organisme -> list -> De top 10 organismen uit de database.
+        aantal_organsime -> list -> Het aantal van de top 10 organismen
+                                uit de database.
+        """
+        # Maakt een connectie met de database.
+        db = mq.connect(host="145.74.104.145",
+                        user="pwtit",
+                        password="pwd123",
+                        database="pwtit",
+                        auth_plugin='mysql_native_password'
+                        )
+        dbcursor = db.cursor()
+        # Haalt de top 10 organisme uit de database
+        organismen = "Select organisme, count(*) From hits Group by " \
+                     "organisme Order by count(*) desc limit 10"
+        dbcursor.execute(organismen)
+        organisme_counter = dbcursor.fetchall()
+        # Zet de resultaten in de goede lijsten
+        for results in organisme_counter:
+            counter = 0
+            for result in results:
+                if counter == 0:
+                    counter += 1
+                    self.__organisme.append(result)
+                elif counter == 1:
+                    self.__aantal_organsime.append(result)
+        return self.__organisme, self.__aantal_organsime
