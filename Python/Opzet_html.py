ee550931d3e545859d48948190152cdc74803a57
NickPolderHAN <n.vandepolder@student.han.nl>
2022-05-31 11:47:42 +0200

    - Minor changes made to opzet_html to fit PEP8

--- Python/Opzet_html.py
+++ Python/Opzet_html.py
@@ -19,18 +19,19 @@ def graphs():
     ei1, ei2, ei3, ei4, ei5, ei6, ei7, ei8, ei9, ei10, = eiwitten
     cei1, cei2, cei3, cei4, cei5, cei6, cei7, cei8, cei9, cei10 = aantal_eiwit
     org1, org2, org3, org4, org5, org6, org7, org8, org9, org10 = organismen
-    corg1, corg2, corg3, corg4, corg5, corg6, corg7, corg8, corg9, \
-    corg10 = aantal_organisme
-    return render_template('graphs.html', ei1=ei1, ei2=ei2, ei3=ei3,\
-                           ei4=ei4, ei5=ei5, ei6=ei6, ei7=ei7, ei8=ei8,\
-                           ei9=ei9, ei10=ei10, org1=org1, org2=org2, \
-                           org3=org3, org4=org4, org5=org5, org6=org6,\
-                           org7=org7, org8=org8, org9=org9, org10=org10,\
-                           cei1=cei1, cei2=cei2, cei3=cei3, cei4=cei4,\
-                           cei5=cei5, cei6=cei6, cei7=cei7, cei8=cei8,\
-                           cei9=cei9, cei10=cei10, corg1=corg1, \
-                           corg2=corg2, corg3=corg3, corg4=corg4, \
-                           corg5=corg5, corg6=corg6, corg7=corg7, \
+    corg1, corg2, corg3, corg4, corg5, corg6, corg7, corg8, corg9,\
+        corg10 = aantal_organisme
+
+    return render_template('graphs.html', ei1=ei1, ei2=ei2, ei3=ei3,
+                           ei4=ei4, ei5=ei5, ei6=ei6, ei7=ei7, ei8=ei8,
+                           ei9=ei9, ei10=ei10, org1=org1, org2=org2,
+                           org3=org3, org4=org4, org5=org5, org6=org6,
+                           org7=org7, org8=org8, org9=org9, org10=org10,
+                           cei1=cei1, cei2=cei2, cei3=cei3, cei4=cei4,
+                           cei5=cei5, cei6=cei6, cei7=cei7, cei8=cei8,
+                           cei9=cei9, cei10=cei10, corg1=corg1,
+                           corg2=corg2, corg3=corg3, corg4=corg4,
+                           corg5=corg5, corg6=corg6, corg7=corg7,
                            corg8=corg8, corg9=corg9, corg10=corg10
                            )
 
