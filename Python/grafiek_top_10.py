8247ec27b669265a38dcdf523c85834aeae0729b
NickPolderHAN <n.vandepolder@student.han.nl>
2022-05-31 12:59:10 +0200

    Minor changes. - Minor changes made to grafiek_top_10

--- Python/grafiek_top_10.py
+++ Python/grafiek_top_10.py
@@ -28,15 +28,16 @@ class TopTen:
                         user="pwtit",
                         password="pwd123",
                         database="pwtit",
-                        auth_plugin='mysql_native_password'
-                        )
+                        auth_plugin='mysql_native_password')
         dbcursor = db.cursor()
         # Haalt de top 10 eiwitten uit de database
         eiwitten = "Select eiwit, count(*) From hits Group by eiwit " \
                    "Order by count(* ) desc limit 10"
+
         dbcursor.execute(eiwitten)
         eiwit_counter = dbcursor.fetchall()
-        # Zet de resultaten in de goede lijsten
+
+        # Zet de resultaten in de juiste lijsten
         for results in eiwit_counter:
             counter = 0
 
@@ -69,21 +70,24 @@ class TopTen:
                         user="pwtit",
                         password="pwd123",
                         database="pwtit",
-                        auth_plugin='mysql_native_password'
-                        )
+                        auth_plugin='mysql_native_password')
         dbcursor = db.cursor()
-        # Haalt de top 10 organisme uit de database
+        # Haalt de top 10 organismen uit de database.
         organismen = "Select organisme, count(*) From hits Group by " \
                      "organisme Order by count(*) desc limit 10"
+
         dbcursor.execute(organismen)
         organisme_counter = dbcursor.fetchall()
-        # Zet de resultaten in de goede lijsten
+
+        # Zet de resultaten in de juiste lijsten.
         for results in organisme_counter:
             counter = 0
+
             for result in results:
                 if counter == 0:
                     counter += 1
                     self.__organism.append(result)
+
                 elif counter == 1:
                     self.__organism_amount.append(result)
 
